8c1b1f0d411f80f93036b0f02045737ce9168125
stm32p103 <stm32p103@gmail.com>
2019-07-30 23:51:07 +0900

    コード生成前登録

---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,31 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'iobpgm' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+/*  ARM::CMSIS:RTOS2:Keil RTX5:Library:5.5.0 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+/*  Keil.STM32CubeMX::Device:STM32Cube Framework:STM32CubeMX:1.0.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/*  Keil.STM32CubeMX::Device:Startup:2.3.3 */
+#define RTE_DEVICE_STARTUP_STM32F0XX    /* Device Startup for STM32F0 */
+/*  Keil::CMSIS Driver:I2C:1.0.0 */
+#define RTE_Drivers_I2C1                /* Driver I2C1 */
+        #define RTE_Drivers_I2C2                /* Driver I2C2 */
+
+
+#endif /* RTE_COMPONENTS_H */
