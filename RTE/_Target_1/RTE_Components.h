7e71703c56123d4c0d2e415546af6fe7d9b7acb3
stm32p103 <stm32p103@gmail.com>
2019-07-31 00:15:46 +0900

    生成ファイル追加

--- RTE/_Target_1/RTE_Components.h
+++ RTE/_Target_1/RTE_Components.h
@@ -21,8 +21,8 @@
         #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
 /*  Keil.STM32CubeMX::Device:STM32Cube Framework:STM32CubeMX:1.0.0 */
 #define RTE_DEVICE_FRAMEWORK_CUBE_MX
-/*  Keil.STM32CubeMX::Device:Startup:2.3.3 */
-#define RTE_DEVICE_STARTUP_STM32F0XX    /* Device Startup for STM32F0 */
+/*  Keil.STM32CubeMX::Device:STM32Cube HAL:1.7.0 */
+#define RTE_DEVICE_CUBE_MX_HAL
 /*  Keil::CMSIS Driver:I2C:1.0.0 */
 #define RTE_Drivers_I2C1                /* Driver I2C1 */
         #define RTE_Drivers_I2C2                /* Driver I2C2 */
